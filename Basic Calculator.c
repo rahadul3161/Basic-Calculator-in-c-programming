496fbd5846cc834637b1119f0f706d89f0f2633b
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-16 00:54:58 +0600

    Add files via upload

---
+++ Basic Calculator.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+    double num1,num2;
+    char operator;
+    printf("<Enter number1>   <then enter operator ('+', '-', '*' or '/')>   <then number2>\n");
+    scanf("%lf%c%lf",&num1,&operator,&num2);
+
+    if (operator=='+')
+        printf("Addition: %.6lf",num1+num2);
+    else if (operator=='-')
+        printf("Subtraction: %.6lf",num1-num2);
+    else if (operator=='*')
+        printf("Multiplication: %.6lf",num1*num2);
+    else if(operator=='/'){
+        if(num2==0)
+            printf("Division: As divisor, zero is not valid!");
+        else
+            printf("Division: %.6lf",num1/num2);}
+    else
+        printf("Invalid operator! Use (+, -, * or /)");
+    return 0;
+}
