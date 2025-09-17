ba099b508254145a53538a554afa47f4b8ed178f
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-09-17 14:20:31 +0600

    Update Basic Calculator-1.c

--- Basic Calculator-1.c
+++ Basic Calculator-1.c
@@ -1,23 +1,31 @@
 #include<stdio.h>
+#include<math.h>
 int main(){
     double num1,num2;
     char operator;
-    printf("<Enter number1>   <then enter operator ('+', '-', '*' or '/')>   <then number2>\n");
-    scanf("%lf%c%lf",&num1,&operator,&num2);
+    printf("<Enter number1>   <then enter ('+','-','*','/' or 'r for reminder')>   <then number2>\n");
+    scanf("%lf %c %lf", &num1, &operator, &num2);
 
     if (operator=='+')
-        printf("Addition: %.6lf",num1+num2);
+        printf("Addition: %lf",num1+num2);
     else if (operator=='-')
-        printf("Subtraction: %.6lf",num1-num2);
+        printf("Subtraction: %lf",num1-num2);
     else if (operator=='*')
-        printf("Multiplication: %.6lf",num1*num2);
+        printf("Multiplication: %lf",num1*num2);
     else if(operator=='/'){
         if(num2==0)
             printf("Division: As divisor, zero is not valid!");
         else
-            printf("Division: %.6lf",num1/num2);}
+            printf("Division: %lf",num1/num2);
+    }
+    else if(operator=='r'){
+        if(num2==0)
+            printf("Remainder: As divisor, zero is not valid!");
+        else
+            printf("Remainder: %lf",fmod(num1,num2));
+    }
     else
-        printf("Invalid operator! Use (+, -, * or /)");
+        printf("Invalid operator! Use (+, -, *, / or r)");
     return 0;
 }
 
